06c605a339824f9f9102db1b94ba3d63abf707ef
Francesco Francomano <silversoulkali@gmail.com>
2019-01-15 23:34:33 +0100

    Initialized login screen with an Icon. ICONS WORKS

--- App.js
+++ App.js
@@ -1,83 +1,11 @@
-import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
-import { connect } from 'react-redux';
+import {Navigation} from 'react-native-navigation';
+import AuthScreen from "./src/screens/Auth/Auth";
 
-import PlaceInput from './src/components/PlaceInput/PlaceInput';
-import PlacesList from './src/components/PlacesList/PlacesList';
-import PlaceDetails from './src/components/PlaceDetails/PlaceDetails';
-import {
-  addPlace,
-  deletePlace,
-  deletePlaceByKey,
-  selectPlace,
-  deselectPlace
-} from './src/store/actions/index';
+Navigation.registerComponent('awesome-places.AuthScreen', () => AuthScreen);
 
-class App extends Component {
-  placeAddedHandler = placeName => {
-    this.props.onAddPlace(placeName);
-  };
-
-  placeDeletedHandler = () => {
-    this.props.onDeletePlace();
-  };
-
-  placeDeletedPlaceByKeyHandler = placeKey => {
-    this.props.onDeletePlaceByKey(placeKey);
-  }
-
-  modalClosedHandler = () => {
-    this.props.onDeselectPlace();
-  };
-
-  placeSelectedHandler = key => {
-    this.props.onSelectPlace(key);
-  };
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <PlaceDetails
-          selectedPlace={this.props.selectedPlace}
-          onItemDeleted={this.placeDeletedHandler}
-          onModalClosed={this.modalClosedHandler}
-        />
-        <PlaceInput onPlaceAdded={this.placeAddedHandler} />
-        <PlacesList
-          places={this.props.places}
-          onItemSelected={this.placeSelectedHandler}
-          onItemLongPressed={this.placeDeletedPlaceByKeyHandler}
-        />
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 26,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'flex-start'
-  }
+Navigation.startSingleScreenApp({
+    screen: {
+        screen: 'awesome-places.AuthScreen',
+        title: 'Auth Screen'
+    }
 });
-
-const mapStateToProps = state => {
-  return {
-    places: state.places.places,
-    selectedPlace: state.places.selectedPlace
-  };
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-    onAddPlace: name => dispatch(addPlace(name)),
-    onDeletePlace: () => dispatch(deletePlace()),
-    onDeletePlaceByKey: placeKey => dispatch(deletePlaceByKey(placeKey)),
-    onSelectPlace: key => dispatch(selectPlace(key)),
-    onDeselectPlace: () => dispatch(deselectPlace())
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
--- index.android.js
+++ index.android.js
@@ -1,30 +1,6 @@
-import React from 'react';
-import { AppRegistry } from 'react-native';
-import { Provider } from 'react-redux';
 import App from './App';
 import { name as appName } from './app.json';
-import configureStore from './src/store/configureStore';
-
-const store = configureStore();
 
 // NB. We don't need this dedicated android/ios index file, I added it just for android/ios customizations
 
-// like a simple constant it will not work
-const RNRedux = () => (
-    <Provider store={store}>
-        <App />
-    </Provider>
-);
-
-// it's ok like this too
-/* class RNRedux extends React.Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <App />
-      </Provider>
-    );
-  }
-} */
-
-AppRegistry.registerComponent(appName, () => RNRedux);
+AppRegistry.registerComponent(appName, () => App);
