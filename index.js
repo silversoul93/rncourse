5624a84fd730f56950f4125d42d15243f91a482b
Francesco Francomano <silversoulkali@gmail.com>
2019-01-17 00:19:39 +0100

    Revert "navigation v2 works"
    This reverts commit 8547c0df1f4fb5baab2a2e2570c92f5efdc30d37.

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 import React from 'react';
-import {Provider} from 'react-redux';
+import { AppRegistry } from 'react-native';
+import { Provider } from 'react-redux';
 import App from './App';
+import { name as appName } from './app.json';
 import configureStore from './src/store/configureStore';
-
-import {Navigation} from 'react-native-navigation';
-import AuthScreen from "./src/screens/Auth/Auth";
 
 const store = configureStore();
 
-Navigation.registerComponent('awesome-places.AuthScreen', () => AuthScreen);
-
-// like a simple constant it will not work
-// const RNRedux = () => (
-//   <Provider store={store}>
-//     <App />
-//   </Provider>
-// );
+// like a simplle constant it will not work
+const RNRedux = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 
 // it's ok like this too
 /* class RNRedux extends React.Component {
@@ -28,12 +25,4 @@ Navigation.registerComponent('awesome-places.AuthScreen', () => AuthScreen);
   }
 } */
 
-Navigation.events().registerAppLaunchedListener(() => {
-    Navigation.setRoot({
-        root: {
-            component: {
-                name: 'awesome-places.AuthScreen'
-            }
-        }
-    });
-});
+AppRegistry.registerComponent(appName, () => RNRedux);
