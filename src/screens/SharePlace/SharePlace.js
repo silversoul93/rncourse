7e01cfacbc5834008892310379a23adf89a2fd53
Francesco Francomano <silversoulKali@gmail.com>
2019-03-21 11:09:51 +0100

    Share place modifications

--- src/screens/SharePlace/SharePlace.js
+++ src/screens/SharePlace/SharePlace.js
@@ -1,9 +1,10 @@
-import React, {Component} from 'react';
-import {View, Keyboard} from 'react-native';
-import PlaceInput from "../../components/PlaceInput/PlaceInput";
-import {connect} from 'react-redux';
-import {addPlace} from '../../store/actions/index';
-import {Navigation} from "react-native-navigation";
+import React, { Component } from "react";
+import { View, Text, TextInput, Button, Keyboard, StyleSheet, ScrollView } from "react-native";
+import { connect } from "react-redux";
+import { addPlace } from "../../store/actions/index";
+import { Navigation } from "react-native-navigation";
+
+import DefaultInput from "../../components/UI/DefaultInput/DefaultInput";
 
 class SharePlaceScreen extends Component {
 
@@ -14,12 +15,12 @@ class SharePlaceScreen extends Component {
 
     navigationButtonPressed({ buttonId }) {
         // will be called when "buttonOne" is clicked
-        Navigation.mergeOptions('MainSideMenu', {
-           sideMenu: {
-               left: {
-                   visible: true
-               }
-           }
+        Navigation.mergeOptions("MainSideMenu", {
+            sideMenu: {
+                left: {
+                    visible: true
+                }
+            }
         });
     }
 
@@ -30,13 +31,44 @@ class SharePlaceScreen extends Component {
 
     render() {
         return (
-          <View>
-              <PlaceInput onPlaceAdded={this.placeAddedHandler}/>
-          </View>
+            <ScrollView>
+                <View style={styles.container}>
+                    <Text>Share a place with us</Text>
+                    <View style={styles.placeholder}>
+                        <Text>Image preview!</Text>
+                    </View>
+
+                    <View>
+                        <Button title="Pick image" onPress={() => alert("onPress")} />
+                    </View>
+
+                    <View style={styles.placeholder}>
+                        <Text>Map</Text>
+                    </View>
+                    
+                    <Button title="Locate me" onPress={() => alert("onPress")} />
+                    <DefaultInput style={{ width: "80%" }} placeholder="place name" />
+                    <Button title="Share the place" onPress={() => alert("onPress")} />
+                </View>
+            </ScrollView>
         );
     }
 
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center'
+    },
+    placeholder: {
+        borderWidth: 1,
+        borderColor: "black",
+        backgroundColor: "#eee",
+        width: "80%",
+        height: 160
+    }
+});
 
 const mapDispatchToProps = dispatch => {
     return {
